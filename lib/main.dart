b762f5cd4097604dba463823f585328adf27c74a
syamsul <80880752+syasul@users.noreply.github.com>
2021-03-30 17:02:31 +0700

    BAPAKMU 5

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:catalog/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
